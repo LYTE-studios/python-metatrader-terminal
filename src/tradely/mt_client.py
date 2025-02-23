abbb27a49f9006ebf54c7496c8497c9f769b05de
FutureSea <futuresea.dev@gmail.com>
2025-02-23 12:39:32 +0200

    add return error issue

--- src/tradely/mt_client.py
+++ src/tradely/mt_client.py
@@ -5,25 +5,29 @@ from datetime import datetime, timedelta
 class MT5Client:
     @staticmethod
     def connect(account, password, server):
-        path = "C:/Program Files/MetaTrader 5/terminal64.exe"
-        if not mt5.initialize(path=path, login=int(account), password=password, server=server):
-            error_code = mt5.last_error()
-            print("initialize() failed, error code =", error_code)
-            mt5.shutdown()
-            return {"status": False, "error_code": str(error_code)}
+        try:
+            path = "C:/Program Files/MetaTrader 5/terminal64.exe"
+            if not mt5.initialize(path=path, login=int(account), password=password, server=server, portable=False):
+                error_code = mt5.last_error()
+                print("initialize() failed, error code =", error_code)
+                mt5.shutdown()
+                return {"status": False, "error_code": str(error_code)}
 
-        authorized = mt5.login(int(account), password=password, server=server)
-        if authorized:
-            account_info = mt5.account_info()
-            return {"status": True, "account_info": account_info}
-        else:
-            error_code = mt5.last_error()
-            print(
-                "failed to connect at account #{}, error code: {}".format(
-                    account, error_code
+            authorized = mt5.login(int(account), password=password, server=server)
+            if authorized:
+                account_info = mt5.account_info()
+                return {"status": True, "account_info": account_info}
+            else:
+                error_code = mt5.last_error()
+                print(
+                    "failed to connect at account #{}, error code: {}".format(
+                        account, error_code
+                    )
                 )
-            )
-            return {"status": False, "error_code": str(error_code)}
+                return {"status": False, "error_code": str(error_code)}
+        except Exception as e:
+            print("Error connecting to MT5: ", e)
+            return {"status": False, "error_code": str(e)}
 
     @staticmethod
     def get_orders(symbol=None, date_from=None, date_to=None, group="GROUP"):
